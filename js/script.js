0c8a96e853797f4036334820ab9b7c90bbb56f57
Wlochp20 <91265023+Wlochp20@users.noreply.github.com>
2022-06-07 17:03:19 +0200

    count end

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const displayTime = document.getElementsByClassName("data")[0];
 const displayDate = document.getElementsByClassName("data")[1];
 let imgName=""
+let imie=""
+
+document.querySelector('.addActivity').addEventListener('click',()=>{
+  document.querySelector(".name img").src="img/a"+imgName+".png"
+  document.querySelector('.activitybtn').src='img/check.png'
+  document.querySelector('.activityModal').style.display="none"
+})
 
 function zegar() {
   date = new Date();
@@ -86,6 +93,8 @@ document.querySelector('.x1img').addEventListener('click', ()=>{
   activityModal.style.display='none';
 })
 let timevalue=30;
+let TimeInterval
+let timevalueconst
 $(function () {
   $("#slider1").roundSlider({
     radius: 80,
@@ -103,39 +112,74 @@ $(function () {
   });
 });
 
-
-for(i=0;i<document.querySelectorAll(".modalOptions img").length;i++){
-  document.querySelectorAll(".modalOptions img")[i].addEventListener('click',function(){
-    imgName=this.getAttribute('alt')
-    document.querySelector(".modalText").innerHTML=imgName
-    document.querySelector(".choosenImg img").src="../img/a"+imgName+".png"
-  })
-}
-document.querySelector('.startButton').addEventListener('click',()=>{
-  document.querySelector(".name span").innerHTML = (document.querySelector(".option span").textContent=="add name") ? "activity":document.querySelector(".option span").textContent;
-  document.querySelector(".name img").src="../img/a"+imgName+".png"
-  document.querySelector(".descriptionText span").innerHTML=(document.querySelector("textarea").value=="")?"no description is here":document.querySelector("textarea").value
-
-  document.querySelector(".countdownModal").style.display='block';
+function startCount(){
   var countdownNumberEl = document.getElementById('countdown-number');
 
   timevalue*=60;
   var countdown = timevalue;
-  var TimeInterval=setInterval(function() {
+  TimeInterval=setInterval(function() {
   minutes = countdown/60;
   seconds=countdown-(Math.floor(minutes)*60)
   countdownNumberEl.textContent = (Math.floor(minutes)<10?"0"+Math.floor(minutes):Math.floor(minutes))+":"+(((seconds)<10) ? "0"+seconds:seconds) 
   console.log(Math.floor(minutes)+" "+(countdown-(Math.floor(minutes)*60)))
   countdown = --countdown
-  if (countdown==0) {
+  if (countdown<0) {
     clearInterval(TimeInterval);
-  }
+    animationRun()
+    document.querySelector(".doneActivityModal-container div img").src=(imgName=="")?"img/check.png": "img/a"+imgName+".png"
+    document.querySelector(".doneActivityModal-container div h3").innerHTML=(imie=="")?"Activity":imie;
+    document.querySelector(".doneActivityModal h4 span").innerHTML=timevalue/60
+    // document.querySelector(".descriptionSpan").innerHTML=
+    document.querySelector(".doneActivityModal").style.display="block"
+    document.querySelector(".countdownModal").style.display="none"
+    }
   }, 1000);
+}
 
+for(i=0;i<document.querySelectorAll(".modalOptions img").length;i++){
+  document.querySelectorAll(".modalOptions img")[i].addEventListener('click',function(){
+    imgName=this.getAttribute('alt')
+    document.querySelector(".modalText").innerHTML=imgName
+    document.querySelector(".choosenImg img").src="img/a"+imgName+".png"
+  })
+}
+document.querySelector('.startButton').addEventListener('click',()=>{
+
+  animationRun(0.3)
+  document.querySelector(".name span").innerHTML = (document.querySelector(".option span").textContent=="add name") ? "activity":document.querySelector(".option span").textContent;
+  document.querySelector(".descriptionText span").innerHTML=(document.querySelector("textarea").value=="")?"no description is here":document.querySelector("textarea").value
+  document.querySelector(".countdownModal").style.display='block';
+
+  startCount()
   document.querySelector(".circle").style.animation =" countdown "+timevalue+"s linear  forwards"
 })
 
 document.querySelector(".addActivity").addEventListener('click',()=>{
   document.querySelector(".activityBtn").src="../img/check.png"
 })
+let animation="";
+function animationRun(time){
+  if (animation!="") {
+    document.querySelector('.animation').remove()
+  }
+  animation=document.createElement('div')
+  animation.className="animation"
+  animation.style.animationDelay=time+"s";
+  let clock=document.createElement('div')
+  clock.className="clock"
+  document.querySelector('body').appendChild(animation)
+  document.querySelector('.animation').appendChild(clock)
+}
+// animationRun(0.5)
+
+function setTime(time){
+  clearInterval(TimeInterval);
+  timevalue=time
+  startCount()
+}
+document.querySelector(".closeBtn").addEventListener('click',()=>{
+  document.querySelector(".doneActivityModal").style.display="none"
+  animationRun(0.2)
+})
+
 
